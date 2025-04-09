dadc523d8dfb9766be7486ef123979e709ad0edb
Iñigo Liberal <inigoliberal@MacBook-Air-de-Inigo.local>
2025-04-09 16:45:01 +0200

    Subida Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { AltaComponent } from './features/users/alta/alta.component';
+import { ListadoComponent } from './features/users/listado/listado.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LoginComponent,
+    AltaComponent,
+    ListadoComponent,
+    NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
